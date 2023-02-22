dd9f0ee29cb35ad22ddb73722be68be783009ece
razaghimahdi <razaghimahdi78@gmail.com>
2023-02-22 12:46:02 +0330

    - Updated MainActivity.kt

--- app/src/main/java/com/razaghimahdi/composepersiandatepicker/MainActivity.kt
+++ app/src/main/java/com/razaghimahdi/composepersiandatepicker/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.razaghimahdi.composepersiandatepicker
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.razaghimahdi.compose_persian_date.PersianDataPickerDialog
+import com.razaghimahdi.compose_persian_date.core.rememberPersianDataPicker
 import com.razaghimahdi.composepersiandatepicker.ui.theme.ComposePersianDatePickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +27,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePersianDatePickerTheme {
-                // A surface container using the 'background' color from the theme
+                val rememberPersianDataPicker = rememberPersianDataPicker()
+                val showDialog = remember { mutableStateOf(false) }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    if (showDialog.value) {
+                        PersianDataPickerDialog(
+                            rememberPersianDataPicker,
+                            Modifier.fillMaxWidth(),
+                            onDismissRequest = { showDialog.value = false },
+                            onDateChanged = { year, month, day ->
+                               // do something...
+                            })
+                    }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        Button(onClick = { showDialog.value = true }) {
+                            Text(text = "نمایش")
+                        }
+
+
+                        Text(text = rememberPersianDataPicker.getPersianLongDate())
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposePersianDatePickerTheme {
-        Greeting("Android")
     }
 }
