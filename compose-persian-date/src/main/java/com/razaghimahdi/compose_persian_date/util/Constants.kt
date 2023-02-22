433fc0fab2df4c27f7307e027dcb0634ef9d9b2b
razaghimahdi <razaghimahdi78@gmail.com>
2023-02-22 12:42:02 +0330

    - Added Constants.kt

---
+++ compose-persian-date/src/main/java/com/razaghimahdi/compose_persian_date/util/Constants.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 razaghimahdi (Mahdi Razzaghi Ghaleh)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.razaghimahdi.compose_persian_date.util
+
+object Constants {
+
+
+    val persianMonthNames = arrayOf(
+        "\u0641\u0631\u0648\u0631\u062f\u06cc\u0646",  // Farvardin
+        "\u0627\u0631\u062f\u06cc\u0628\u0647\u0634\u062a",  // Ordibehesht
+        "\u062e\u0631\u062f\u0627\u062f",  // Khordad
+        "\u062a\u06cc\u0631",  // Tir
+        "\u0645\u0631\u062f\u0627\u062f",  // Mordad
+        "\u0634\u0647\u0631\u06cc\u0648\u0631",  // Shahrivar
+        "\u0645\u0647\u0631",  // Mehr
+        "\u0622\u0628\u0627\u0646",  // Aban
+        "\u0622\u0630\u0631",  // Azar
+        "\u062f\u06cc",  // Dey
+        "\u0628\u0647\u0645\u0646",  // Bahman
+        "\u0627\u0633\u0641\u0646\u062f" // Esfand
+    )
+
+}
