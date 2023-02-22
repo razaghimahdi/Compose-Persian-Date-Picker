f0b7e71e5491cbcd5f44105b996cd46acb17ecad
razaghimahdi <razaghimahdi78@gmail.com>
2023-02-22 12:45:19 +0330

    - Added PersianDataPicker.kt and PersianDataPickerDialog.kt

---
+++ compose-persian-date/src/main/java/com/razaghimahdi/compose_persian_date/PersianDataPicker.kt
@@ -0,0 +1,194 @@
+/*
+ * Copyright (C) 2022 razaghimahdi (Mahdi Razzaghi Ghaleh)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.razaghimahdi.compose_persian_date
+
+import android.graphics.Typeface
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.NumberPicker
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.razaghimahdi.compose_persian_date.GlobalStyle.textColor
+import com.razaghimahdi.compose_persian_date.GlobalStyle.textSize
+import com.razaghimahdi.compose_persian_date.GlobalStyle.typeface
+import com.razaghimahdi.compose_persian_date.core.CustomNumberPicker
+import com.razaghimahdi.compose_persian_date.core.NumberPicker
+import com.razaghimahdi.compose_persian_date.core.PersianDataPickerController
+import com.razaghimahdi.compose_persian_date.util.Constants.persianMonthNames
+import com.razaghimahdi.compose_persian_date.util.Tools.changeDividerColor
+import com.razaghimahdi.compose_persian_date.util.Tools.getStringColor
+import com.razaghimahdi.compose_persian_date.util.Tools.toPersianNumber
+
+
+@Composable
+internal fun PersianDataPicker(
+    controller: PersianDataPickerController,
+    modifier: Modifier = Modifier,
+    onDateChanged: ((year: Int, month: Int, day: Int) -> Unit)? = null
+) {
+
+    controller.initDate()
+
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+
+        ComposeCustomNumberPicker(
+            modifier = Modifier.wrapContentWidth(),
+            formatter = { i -> i.toString().toPersianNumber() },
+            onValueChangedListener = { picker, oldVal, newVal ->
+                controller.updateFromCustomNumberPicker(newDay = newVal)
+                if (onDateChanged != null) {
+                    onDateChanged(
+                        controller.selectedYear,
+                        controller.selectedMonth,
+                        controller.selectedDay
+                    )
+                }
+
+            },
+            minValue = 1,
+            maxValue = controller.maxDay,
+            selectedValue = controller.selectedDay,
+        )
+
+        Spacer(
+            modifier = Modifier
+                .height(4.dp)
+                .width(4.dp)
+        )
+
+        ComposeCustomNumberPicker(
+            modifier = Modifier.wrapContentWidth(),
+            formatter = { i -> i.toString().toPersianNumber() },
+            onValueChangedListener = { picker, oldVal, newVal ->
+                controller.updateFromCustomNumberPicker(newMonth = newVal)
+                if (onDateChanged != null) {
+                    onDateChanged(
+                        controller.selectedYear,
+                        controller.selectedMonth,
+                        controller.selectedDay
+                    )
+                }
+            },
+            minValue = 1,
+            maxValue = if (controller.maxMonth > 0) controller.maxMonth else 12,
+            selectedValue = controller.selectedMonth,
+            displayedValues = if (controller.displayMonthNames) persianMonthNames else null
+        )
+
+        Spacer(
+            modifier = Modifier
+                .height(4.dp)
+                .width(4.dp)
+        )
+
+        ComposeCustomNumberPicker(
+            modifier = Modifier.wrapContentWidth(),
+            formatter = { i -> i.toString().toPersianNumber() },
+            onValueChangedListener = { picker, oldVal, newVal ->
+                controller.updateFromCustomNumberPicker(newYear = newVal)
+                if (onDateChanged != null) {
+                    onDateChanged(
+                        controller.selectedYear,
+                        controller.selectedMonth,
+                        controller.selectedDay
+                    )
+                }
+            },
+            minValue = controller.minYear,
+            maxValue = controller.maxYear,
+            selectedValue = controller.selectedYear
+        )
+
+    }
+
+}
+
+
+@Composable
+fun ComposeCustomNumberPicker(
+    modifier: Modifier,
+    formatter: NumberPicker.Formatter,
+    onValueChangedListener: NumberPicker.OnValueChangeListener,
+    minValue: Int,
+    maxValue: Int,
+    selectedValue: Int,
+    // dividerColor: Color = Color.Red,
+    displayedValues: Array<String>? = null,
+    textStyle: TextStyle = LocalTextStyle.current
+) {
+
+
+    textSize = textStyle.fontSize.value
+    textColor = android.graphics.Color.parseColor(getStringColor(textStyle.color))
+
+
+    /* NumberPicker(
+         value = selectedValue,
+         range = minValue..maxValue,
+         onValueChange = {
+             pickerValue = it
+         }
+     )*/
+
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+
+            val numberPicker = CustomNumberPicker(context = context)
+            /* changeDividerColor(
+                 numberPicker,
+                 android.graphics.Color.parseColor(getStringColor(dividerColor))
+             )*/
+            numberPicker.setFormatter(formatter)
+            numberPicker.minValue = minValue
+            numberPicker.maxValue = maxValue
+            numberPicker.value = selectedValue
+            numberPicker.setOnValueChangedListener(onValueChangedListener)
+            if (displayedValues != null) numberPicker.displayedValues = displayedValues
+
+
+            numberPicker
+        },
+        update = { numberPicker ->
+            numberPicker.minValue = minValue
+            numberPicker.maxValue = maxValue
+            numberPicker.value = selectedValue
+
+        }
+    )
+}
+
+internal object GlobalStyle {
+
+    internal var typeface: Typeface? = null
+    internal var textSize: Float? = null
+    internal var textColor: Int? = null
+
+}
---
+++ compose-persian-date/src/main/java/com/razaghimahdi/compose_persian_date/PersianDataPickerDialog.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2022 razaghimahdi (Mahdi Razzaghi Ghaleh)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.razaghimahdi.compose_persian_date
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import com.razaghimahdi.compose_persian_date.core.NoPaddingAlertDialog
+import com.razaghimahdi.compose_persian_date.core.PersianDataPickerController
+import java.util.*
+
+@Composable
+fun PersianDataPickerDialog(
+    controller: PersianDataPickerController,
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit,
+    onDateChanged: ((year: Int, month: Int, day: Int) -> Unit)? = null,
+    // submitTitle: String = "تایید",
+    dismissTitle: String = "بستن",
+    textButtonStyle: TextStyle = LocalTextStyle.current,
+    shape: Shape = MaterialTheme.shapes.medium,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    properties: DialogProperties = DialogProperties()
+) {
+
+    NoPaddingAlertDialog(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        shape = shape,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        properties = properties,
+        title = { },
+        text = { PersianDataPicker(controller, modifier.padding(8.dp), onDateChanged) },
+        buttons = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(onClick = { controller.resetDate() }) {
+                    Text(text = "امروز", style = textButtonStyle)
+                }
+                Row(
+                    modifier = Modifier.wrapContentWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    TextButton(onClick = onDismissRequest) {
+                        Text(text = dismissTitle, style = textButtonStyle)
+                    }
+                    /* TextButton(onClick = {
+                         onDismissRequest()
+                     }) {
+                         Text(text = submitTitle)
+                     }*/
+                }
+
+            }
+        },
+    )
+
+
+}
