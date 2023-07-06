12b273b5dfda02cb1ce0e23c6f060c0c50ff4c79
razaghimahdi <razaghimahdi78@gmail.com>
2023-07-06 22:15:51 +0330

    - Updated Tools.kt: added new fun to change color of number picker text

--- compose-persian-date/src/main/java/com/razaghimahdi/compose_persian_date/util/Tools.kt
+++ compose-persian-date/src/main/java/com/razaghimahdi/compose_persian_date/util/Tools.kt
@@ -18,10 +18,15 @@
 package com.razaghimahdi.compose_persian_date.util
 
 import android.content.res.Resources.NotFoundException
+import android.graphics.Paint
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.view.View
+import android.widget.EditText
 import android.widget.NumberPicker
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import java.lang.reflect.Field
 
 
 internal object Tools {
@@ -102,4 +107,34 @@ internal object Tools {
         }
     }
 
+}
+
+
+internal fun NumberPicker.changeTextColor(color: Int) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        val count: Int = childCount
+        for (i in 0 until count) {
+            val child: View = getChildAt(i)
+            if (child is EditText) {
+                try {
+                    child.setTextColor(color)
+                    invalidate()
+                    val selectorWheelPaintField: Field = this.javaClass.getDeclaredField("mSelectorWheelPaint")
+                    var accessible: Boolean = selectorWheelPaintField.isAccessible
+                    selectorWheelPaintField.isAccessible = true
+                    (selectorWheelPaintField.get(this) as Paint).color = color
+                    selectorWheelPaintField.isAccessible = accessible
+                    invalidate()
+                    val selectionDividerField: Field = this.javaClass.getDeclaredField("mSelectionDivider")
+                    accessible = selectionDividerField.isAccessible()
+                    selectionDividerField.isAccessible = true
+                    selectionDividerField.set(this, null)
+                    selectionDividerField.isAccessible = accessible
+                    invalidate()
+                } catch (ignore: Exception) { }
+            }
+        }
+    } else {
+        textColor = color
+    }
 }
