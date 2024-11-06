92000a52a88f21df1851ec17d114f4cb482dd9e8
mahdirazzaghi <razaghimahdi78@gmail.com>
2024-11-06 19:45:57 +0330

    - Added PDate.kt

---
+++ compose-persian-date/src/main/java/com/razaghimahdi/compose_persian_date/core/model/PDate.kt
@@ -0,0 +1,9 @@
+package com.razaghimahdi.compose_persian_date.core.model
+
+import saman.zamani.persiandate.PersianDate
+
+data class PDate(
+    val value: Int,
+    val persianDate: PersianDate,
+    val isSelected: Boolean
+)
