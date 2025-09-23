8495f63a9f387a89ca1e444b9bd045701ee6fbf7
mahdirazzaghi <razaghimahdi78@gmail.com>
2025-09-23 10:15:24 +0330

    - Updated MainActivity.kt

--- app/src/main/java/com/razaghimahdi/composepersiandatepicker/MainActivity.kt
+++ app/src/main/java/com/razaghimahdi/composepersiandatepicker/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheetProperties
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -24,12 +23,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.SecureFlagPolicy
-import com.razaghimahdi.compose_persian_date.bottom_sheet.DatePickerLinearModalBottomSheet
-import com.razaghimahdi.compose_persian_date.calendar_date_picker.RangeDatePicker
+import com.razaghimahdi.compose_persian_date.bottom_sheet.linear.DatePickerLinearModalBottomSheet
+import com.razaghimahdi.compose_persian_date.bottom_sheet.single.DatePickerSingleModalBottomSheet
 import com.razaghimahdi.compose_persian_date.core.controller.rememberDialogDatePicker
-import com.razaghimahdi.compose_persian_date.core.controller.rememberPersianRangeDatePickerController
+import com.razaghimahdi.compose_persian_date.core.controller.rememberPersianSingleDatePickerController
 import com.razaghimahdi.compose_persian_date.dialog.PersianLinearDatePickerDialog
+import com.razaghimahdi.compose_persian_date.dialog.SingleDatePickerDialog
 import com.razaghimahdi.composepersiandatepicker.ui.theme.ComposePersianDatePickerTheme
 import kotlinx.coroutines.launch
 import java.util.Date
@@ -44,12 +43,24 @@ class MainActivity : ComponentActivity() {
             ComposePersianDatePickerTheme {
                 val coroutine = rememberCoroutineScope()
                 val rememberPersianDialogDatePicker = rememberDialogDatePicker()
+                val rememberPersianDialogSingleDatePickerController = rememberPersianSingleDatePickerController()
+                val rememberPersianBottomSheetSingleDatePickerController = rememberPersianSingleDatePickerController()
                 val rememberPersianBottomSheetDatePickerController = rememberDialogDatePicker()
-                val showDialog = remember { mutableStateOf(false) }
-                val bottomSheetState = rememberModalBottomSheetState()
+                val showLinearDialog = remember { mutableStateOf(false) }
+                val bottomLinearSheetState = rememberModalBottomSheetState()
+                val showSingleDialog = remember { mutableStateOf(false) }
+                val bottomSingleSheetState = rememberModalBottomSheetState()
 
 
                 LaunchedEffect(key1 = Unit) {
+
+                    rememberPersianDialogSingleDatePickerController.updateDate(date = Date())
+                    rememberPersianDialogSingleDatePickerController.updateDate(timestamp = Date().time)
+                    rememberPersianDialogSingleDatePickerController.updateDate(
+                        persianYear = 1403,
+                        persianMonth = 7,
+                        persianDay = 20
+                    )
 
                     rememberPersianDialogDatePicker.updateDate(date = Date())
                     rememberPersianDialogDatePicker.updateDate(timestamp = Date().time)
@@ -66,6 +77,13 @@ class MainActivity : ComponentActivity() {
                         persianMonth = 7,
                         persianDay = 20
                     )
+                    rememberPersianBottomSheetSingleDatePickerController.updateDate(date = Date())
+                    rememberPersianBottomSheetSingleDatePickerController.updateDate(timestamp = Date().time)
+                    rememberPersianBottomSheetSingleDatePickerController.updateDate(
+                        persianYear = 1403,
+                        persianMonth = 7,
+                        persianDay = 20
+                    )
                 }
 
 
@@ -78,11 +96,11 @@ class MainActivity : ComponentActivity() {
                 rememberPersianBottomSheetDatePickerController.updateMaxYear(1420)
                 rememberPersianBottomSheetDatePickerController.updateMinYear(1395)
 
-                if (showDialog.value) {
+                if (showLinearDialog.value) {
                     PersianLinearDatePickerDialog(
                         rememberPersianDialogDatePicker,
                         Modifier.fillMaxWidth(),
-                        onDismissRequest = { showDialog.value = false },
+                        onDismissRequest = { showLinearDialog.value = false },
                         onDateChanged = { year, month, day ->
                             // do something...
                             Log.i(
@@ -91,16 +109,42 @@ class MainActivity : ComponentActivity() {
                             )
                         })
                 }
+                if (showSingleDialog.value) {
+                    SingleDatePickerDialog(
+                        rememberPersianDialogSingleDatePickerController,
+                        Modifier.fillMaxWidth(),
+                        onDismissRequest = { showSingleDialog.value = false },
+                        onDateChanged = { year, month, day ->
+                            // do something...
+                            Log.i(
+                                "TAG",
+                                "onCreate getPersianFullDate: " + rememberPersianDialogSingleDatePickerController.getPersianFullDate()
+                            )
+                        })
+                }
 
-                if (bottomSheetState.isVisible) {
+                if (bottomLinearSheetState.isVisible) {
                     DatePickerLinearModalBottomSheet(
                         modifier = Modifier
                             .fillMaxSize(),
-                        sheetState = bottomSheetState,
+                        sheetState = bottomLinearSheetState,
                         controller = rememberPersianBottomSheetDatePickerController,
                         onDismissRequest = {
                             coroutine.launch {
-                                bottomSheetState.hide()
+                                bottomLinearSheetState.hide()
+                            }
+                        }
+                    )
+                }
+                if (bottomSingleSheetState.isVisible) {
+                    DatePickerSingleModalBottomSheet(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        sheetState = bottomSingleSheetState,
+                        controller = rememberPersianBottomSheetSingleDatePickerController,
+                        onDismissRequest = {
+                            coroutine.launch {
+                                bottomSingleSheetState.hide()
                             }
                         }
                     )
@@ -117,20 +161,33 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
 
-                        RangeDatePicker(rememberPersianRangeDatePickerController())
 
-
-                        Button(onClick = { showDialog.value = true }) {
-                            Text(text = "نمایش دیالوگ")
+                        Button(onClick = { showLinearDialog.value = true }) {
+                            Text(text = "نمایش دیالوگ دیت پیکر")
                         }
                         Text(text = rememberPersianDialogDatePicker.getPersianFullDate())
 
-                        Spacer(modifier = Modifier.size(32.dp))
+                        Spacer(modifier = Modifier.size(8.dp))
 
-                        Button(onClick = { coroutine.launch { bottomSheetState.show() } }) {
-                            Text(text = "نمایش باتم شت")
+
+                        Button(onClick = { showSingleDialog.value = true }) {
+                            Text(text = "نمایش دیالوگ سینگل دیت پیکر")
+                        }
+                        Text(text = rememberPersianDialogSingleDatePickerController.getPersianFullDate())
+
+                        Spacer(modifier = Modifier.size(8.dp))
+
+                        Button(onClick = { coroutine.launch { bottomLinearSheetState.show() } }) {
+                            Text(text = "نمایش باتم شت دیت پیکر")
                         }
                         Text(text = rememberPersianBottomSheetDatePickerController.getPersianFullDate())
+
+                        Spacer(modifier = Modifier.size(8.dp))
+
+                        Button(onClick = { coroutine.launch { bottomSingleSheetState.show() } }) {
+                            Text(text = "نمایش باتم شت سینگل دیت پیکر")
+                        }
+                        Text(text = rememberPersianBottomSheetSingleDatePickerController.getPersianFullDate())
                     }
 
                 }
